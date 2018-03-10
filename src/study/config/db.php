e380d9ba982efddeaef7c7f779aa19697a2dcbd2
Aritomo <aritomo.san@gmail.com>
2018-03-10 23:46:12 +0300

    Initial commit

---
+++ src/study/config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mariadb;dbname=nameDB',
+    'username' => 'admin',
+    'password' => 'passDB',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
---
+++ src/study/migrations/m180310_195705_mama_mia.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180310_195705_mama_mia
+ */
+class m180310_195705_mama_mia extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m180310_195705_mama_mia cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m180310_195705_mama_mia cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
