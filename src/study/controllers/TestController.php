4e6b2c1dcf2d0d064ba798284ae6ee55a725fd05
Aritomo <aritomo.san@gmail.com>
2018-03-10 23:52:43 +0300

    hell world

---
+++ src/study/controllers/TestController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arito
+ * Date: 10.03.2018
+ * Time: 23:51
+ */
+
+namespace app\controllers;
+
+
+class TestController
+{
+
+}
---
+++ src/study/views/site/new.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arito
+ * Date: 10.03.2018
+ * Time: 23:52
+ */
